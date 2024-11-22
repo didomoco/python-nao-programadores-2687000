06e94f5f0a587163687da4ebd24c0d2446dfad09
Diego Coelho <75261896+didomoco@users.noreply.github.com>
2024-11-22 22:04:57 +0000

    calculos numericos

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,14 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print(f"O valor é {55 + 99 - 9}") 
 
 # Realize uma operação entre dados do tipo inteiro e float
+print(f"o calculo é {idade_numerica * altura}")
+print(type(f"o calculo é {idade_numerica * altura}"))
+print(idade_numerica * altura)
