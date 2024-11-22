c31c383bfefc03452d86da211d0294d1fd4f37d6
Diego Coelho <75261896+didomoco@users.noreply.github.com>
2024-11-22 21:25:38 +0000

    Declaracao de variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,15 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome_masculino = 'Joao'
+nome_feminino = 'Maria'
 
 # Declare variáveis de cada tipo numérico
 
+numero_inteiro = 1
+numero_decimal = 3.7
 
 # Declare uma variável do tipo booleano
+
+Acionar_Engrenagem = True
+Desativar_Engrenagem = False
